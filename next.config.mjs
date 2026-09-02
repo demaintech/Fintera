ef5fa7f91af3b74e908773b7cd42fdb63e18c1e9
Demain <demainkkinc@gmail.com>
2026-09-02 18:13:19 +0100

    Fixed vercel deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -66,16 +66,6 @@ const nextConfig = {
     ];
   },
 
-  // Redirects
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/admin',
-  //       destination: '/admin/analytics',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
